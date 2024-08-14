598f37beae886e0b89c6d0546e48e7c153916f50
bhavilad <bhavi.ld@gmail.com>
2024-08-14 16:59:02 +0400

    Added user interface, api call using Async await, and a model

--- SwiftUIAsyncAwait/ContentView.swift
+++ SwiftUIAsyncAwait/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var user: GitHubUser?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            
+            AsyncImage(url: URL(string: user?.avatarUrl ?? "")) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(Circle())
+            } placeholder: {
+                Circle()
+                    .foregroundColor(.secondary)
+            }
+            .frame(width: 120, height: 120)
+
+            Text(user?.login ?? "Username")
+                .bold()
+                .font(.title)
+            Text(user?.bio ?? "this is the description.")
+                .padding()
+            Spacer()
         }
         .padding()
+        .task {
+            do {
+                user = try await NetworkManager.shared.getUser()
+            } catch APIError.invalidURL {
+                print("Invalid URL")
+            } catch APIError.invalidResponse {
+                print("Invalid Response")
+            } catch APIError.invalidData {
+                print("Invalid Data")
+            } catch {
+                print("Unexpected Error")
+            }
+        }
     }
 }
 
