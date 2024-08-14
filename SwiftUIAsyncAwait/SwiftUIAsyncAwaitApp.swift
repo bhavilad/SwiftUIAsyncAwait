f88c4e864856a95deeac08ad77f42422c9d6f095
bhavilad <bhavi.ld@gmail.com>
2024-08-14 11:33:01 +0400

    Initial Commit

---
+++ SwiftUIAsyncAwait/SwiftUIAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncAwaitApp.swift
+//  SwiftUIAsyncAwait
+//
+//  Created by Bhavi Mistry on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
